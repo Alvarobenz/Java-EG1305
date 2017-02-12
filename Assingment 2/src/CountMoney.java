1e84b942afafe502d9b98a130b81ffe13fbd5ded
aktunc-STMU <aktunc@gmail.com>
2017-02-12 11:25:28 -0600

    Check comments on formatting

--- Assingment 2/src/CountMoney.java
+++ Assingment 2/src/CountMoney.java
@@ -1,3 +1,6 @@
+/* Format your code using Ctrl+Shift+F in Eclipse
+Leave spaces on both sides of an operator
+*/
 import java.util.Scanner;
 
 public class CountMoney {
@@ -14,7 +17,7 @@ public class CountMoney {
 		int cents = integer1 % 100;
 		int quarters = cents / 25;		
 		int dimes = (cents - quarters*25) / 10;
-		int nickels = (cents - (quarters*25) - (dimes*10)) / 5;
+		int nickels = (cents - (quarters*25) - (dimes*10)) / 5; //Use spaces (quarters * 25)
 		int pennies = (cents - (quarters*25) - (dimes*10) - (nickels*5)) / 1;
 		
 		System.out.println("Your amount " + integer1 + " consists of\n" + dollars + "   dollars\n" 
