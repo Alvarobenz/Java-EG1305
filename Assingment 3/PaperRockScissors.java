66c6500386ea5c7b75b47c21d342b058b091f8f4
Alvaro B <alvaro.benz@gmail.com>
2017-02-15 16:53:14 -0600

    Assingment 3 complete

--- Assingment 3/PaperRockScissors.java
+++ Assingment 3/PaperRockScissors.java
@@ -6,8 +6,13 @@ public class PaperRockScissors {
 		//RNG
 		Random rand = new Random();
 		
-		int playermove, cmpmove;
+		int playerMove, cmpMove;
 		int win = 0;
+		
+		//String copies to store string versions of 0,1,2
+		String playerMoveSt = "";
+		String cmpMoveSt = "";
+		
 		String result = "";
 		String winner= ""; 
 		String looser = "";
@@ -16,49 +21,80 @@ public class PaperRockScissors {
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("scissors (0), rock(1), paper(2): ");
-		playermove = scan.nextInt();
+		playerMove = scan.nextInt();
 		
 		//RNG 50 is the max and 1 is min
-		cmpmove = rand.nextInt(3) + 1;
+		cmpMove = rand.nextInt(3) + 1;
 		
 		//Determine who wins and update win
-		if (playermove == cmpmove){
+		if (playerMove == cmpMove){
 			gamestatus = "ties";
 			result = "tied";
 			win = 2;		
 		}
-		else if (playermove == 0 &&  cmpmove == 2) {
+		else if (playerMove == 0 &&  cmpMove == 2) {
 			win = 1;
 			result = "won";
 		}
-		else if (playermove == 1 && cmpmove == 0) {
+		else if (playerMove == 1 && cmpMove == 0) {
 			win = 1;
 			result = "won";
 		}
-		else if (playermove == 2 && cmpmove == 1) {
+		else if (playerMove == 2 && cmpMove == 1) {
 			win = 1;
 			result = "won";
 		}
-		else if (playermove == 0 && cmpmove == 1) {
+		else if (playerMove == 0 && cmpMove == 1) {
 			win = 0;
 			result = "lost";
 		}
-		else if (playermove == 1 && cmpmove == 2 ) {
+		else if (playerMove == 1 && cmpMove == 2 ) {
 			win = 0;
 			result = "lost";
 		}
-		else if (playermove == 2 && cmpmove == 0) {
+		else if (playerMove == 2 && cmpMove == 0) {
 			win = 0;
 			result = "lost";
 		}
-	
+		
+		//Updates string version of playerMoveSt
+		switch(playerMove){
+			case 0:
+			playerMoveSt = "Scissors";
+			break;
+			case 1:
+			playerMoveSt = "Rock";
+			break;
+			case 2:
+			playerMoveSt = "Paper";
+		}
+		
+		//Updates string version of cmpMoveSt
+		switch(cmpMove){
+			case 0:
+			cmpMoveSt = "Scissors";
+			break;
+			case 1:
+			cmpMoveSt = "Rock";
+			break;
+			case 2:
+			cmpMoveSt = "Paper";
+		}
 		switch(win){
 			case 0:
-			winner = ;
-		
+			winner = cmpMoveSt;
+			looser = playerMoveSt;
+			break;
+			case 1:
+			winner = playerMoveSt;
+			looser = cmpMoveSt;
+			break;
+			case 2:
+			winner = playerMoveSt;
+			looser = cmpMoveSt;
 		}
-			
-			
+		
+	
 		System.out.printf("You %s: %s %s %s", result, winner, gamestatus, looser);
 	
 
