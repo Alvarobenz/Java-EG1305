e65ba2377bcf94bcf44feacef4551b5090aebcf1
Alvaro B <alvaro.benz@gmail.com>
2017-02-25 14:58:31 -0600

    Update 2

---
+++ Assingment 4/NumberGuess.java
@@ -0,0 +1,9 @@
+
+public class NumberGuess {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a number between 1 and 100" + "\n");
+//
+	}
+
+}
---
+++ Assingment 4/Prime.java
@@ -0,0 +1,9 @@
+
+public class Prime {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a number. The program will print all the prime numbers than this number\n");
+
+	}
+
+}
