0aed5363897689e89ead2e61132148ab8393ad6c
Alvaro B <alvaro.benz@gmail.com>
2017-02-28 13:48:26 -0600

    Assingment 4 udpates1

--- Assingment 4/Prime.java
+++ Assingment 4/Prime.java
@@ -1,9 +1,47 @@
+import java.util.*;
 
 public class Prime {
 
 	public static void main(String[] args) {
-		System.out.println("Enter a number. The program will print all the prime numbers than this number\n");
+		
+		//Initialize values
+		Scanner scan = new Scanner(System.in);
+	
+		int i,j;
+		int prime = 1, divisor = 0;
+		int userInput = 0;
+		
 
+		System.out.println("Enter a number. The program will print all the prime numbers than this number");
+		
+		
+		//Store userInput
+		userInput = scan.nextInt();
+		
+
+		for(i=2; i < userInput; i++) {
+			
+			System.out.println(i + "ex");
+			//Highest possible value to check if multiple
+			divisor = i / 2;
+			
+			for(j=2; j<=divisor; j++) {
+				if( i%j == 0) {
+					prime = 0;
+					break;
+				}
+			}//outer for
+			
+			
+			//If prime
+			if(prime == 1) {
+				System.out.printf(i + "\t");			
+			}
+			
+			
+		}//end outer for
+	
+		
+		
 	}
-
 }
