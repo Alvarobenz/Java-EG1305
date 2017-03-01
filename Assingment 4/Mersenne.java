0b9c39a34dc8b8496ca7bd7795b8a333b5e3ed6c
Alvaro B <alvaro.benz@gmail.com>
2017-03-01 16:32:53 -0600

    Assingment 4 final

--- Assingment 4/Mersenne.java
+++ Assingment 4/Mersenne.java
@@ -12,7 +12,6 @@ public class Mersenne {
 		double mersenne = 0;
 		int mersenneCount = 0;
 		int userInput = 0;
-		
 
 		System.out.println("Enter a number. The program will print all the Mersenne prime numbers than this number");
 		
@@ -20,23 +19,24 @@ public class Mersenne {
 		//Store userInput
 		userInput = scan.nextInt();
 		
-		
-		while(mersenne <= userInput) {
-			i = 2;
-					
+		i = 2;
+		while(mersenne < userInput) {
+			
 			if(isprime(i)){
 				mersenne = ( Math.pow(2, i) ) - 1;
 				
-				if(isprime((int)mersenne)){
-					System.out.println(mersenne +"\t");
-					mersenneCount++;
-				}
-				
+				if(mersenne < userInput) {
+					
+					if(isprime((int)mersenne)){
+						System.out.printf((int)mersenne + "\t");
+						mersenneCount++;
+					}//inner
+				}//outer
 			}//outer if
 			i++;
-		}//outer for
+		}
 		
-		System.out.println("There are a total of " + mersenne + " prime numbers" );
+		System.out.println("\nThere are a total of " + mersenneCount + " mersenne prime numbers" );
 		
 
 	}
