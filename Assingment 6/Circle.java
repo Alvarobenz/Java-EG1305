c7784e42de27c4e91a66a6a44f7c1407f96e3574
Alvaro B <alvaro.benz@gmail.com>
2017-03-24 16:08:24 -0500

    Assingment 6

---
+++ Assingment 6/Circle.java
@@ -0,0 +1,15 @@
+
+public class Circle {
+	
+	double radius;
+	
+	
+	public Circle1() {
+		
+	}
+	
+	public Circle1( int a, int b){
+		
+	}
+
+}
--- Assingment 6/Distance.java
+++ Assingment 6/Distance.java
@@ -1,11 +1,32 @@
+import java.util.*;
+import java.lang.*;
 
 public class Distance {
 
 	public static void main(String[] args) {
 		
-		System.out.print("Enter x1 & y1");
-		\
+		Scanner scan = new Scanner(System.in);
 
+		System.out.print("Enter x1: ");
+		int x1 = scan.nextInt();
+		System.out.print("Enter y1: ");
+		int y1 = scan.nextInt();
+		
+		System.out.print("Enter x2: ");
+		int x2 = scan.nextInt();
+		System.out.print("Enter y2: ");
+		int y2 = scan.nextInt();
+		
+		System.out.println("Disance is: " + getDistance(x1, y1, x2, y2));
+		
+	}
+	
+	
+	
+	public static double getDistance(int x1, int y1, int x2, int y2){
+		
+		return Math.sqrt(Math.pow( (x2 - x1), 2) + Math.pow( (y2 - y1), 2)) ;
+		
 	}
 
 }
