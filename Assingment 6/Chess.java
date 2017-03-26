5f98a166b5d9b96af11de03158afa1990fba164d
Alvaro B <alvaro.benz@gmail.com>
2017-03-26 13:41:15 -0500

    Assingment 6

--- Assingment 6/Chess.java
+++ Assingment 6/Chess.java
@@ -1,23 +1,39 @@
 import java.util.*;
 import java.lang.*;
+import java.math.*;
 
 public class Chess {
 
 	public static void main(String[] args) {
 
-		long totalGrains = 1;
+		BigInteger totalGrains = new BigInteger("0");
 		long finalGrains = 0;
 		int chessBoard = 64;
 	
 		
-		for(int i = 1; i <= chessBoard; i++) {
-			totalGrains += Math.pow(2, i) - 1;
+		for(int i = 0; i <=chessBoard; i++) {
+			
+			double calc1 = (Math.pow(2, i));
+			
+			System.out.println("calc1: " + calc1);
+			
+			String grain = removeChar(Double.toString(calc1));
+			
+			System.out.println("Ex: " + grain);
+			
+			BigInteger temp1 = new BigInteger(grain);
+					
+			totalGrains = totalGrains.add(temp1);
 			
 		}
 		
 		finalGrains = (long)Math.pow(2, chessBoard);
 		
 		System.out.println("Total grains: " + totalGrains + "\n" + "Final grains: " + finalGrains);
+	}
+	
+	private static String removeChar(String str){
+		return str.substring(0, str.length()-2);
 	}
 	
 }
--- Assingment 6/Distance.java
+++ Assingment 6/Distance.java
@@ -30,3 +30,4 @@ public class Distance {
 	}
 
 }
+
