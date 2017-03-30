baa0c4b709204da0842faf81744738a953957200
Alvaro B <alvaro.benz@gmail.com>
2017-03-30 03:04:01 -0500

    Assingment 7

--- Assingment 7/Account.java
+++ Assingment 7/Account.java
@@ -7,14 +7,55 @@ public class Account {
 	private String dateCreated;
 	
 	
-	public Account(int id, double balance, double annualInterestRate){
-	
+	public Account(int id1, double balance1, double annualInterestRate1){
+		id1 = id;
+		balance1 = balance;
+		annualInterestRate1 = annualInterestRate;
 	}
 	
-	public void setid(){
+	public void setId(){
 		
 		
 	}
+	
+	public int getId(){
+		
+	}
+	
+	public void setBalance(){
+		
+	}
+	
+	public double getBalance(){
+		
+	}
+	
+	public void annualInterestRate(){
+		
+	}
+	
+	public double annualInterestRate(){
+		
+	}
+	
+	public String dateCreated(){
+		
+	}
+	
+	public double getMonthlyInterestRate(){
+		
+	}
+	
+	public void withdraw(double cash){
+		
+	}
+	
+	public void deposite(double cash){
+		
+	}
+	
+	                                                                                                                                                                                                                                                                                                                                                                                                      
+	
 	
 	public Account(){
 		
